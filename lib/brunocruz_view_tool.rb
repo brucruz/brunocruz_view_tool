0053e1a7717af304c5f2b0244303dde8911e754c
Bruno <bru.cruz@gmail.com>
2017-11-29 15:14:43 -0200

    BrunoCruz initial commit

---
+++ lib/brunocruz_view_tool.rb
@@ -0,0 +1,6 @@
+require "brunocruz_view_tool/version"
+require "brunocruz_view_tool/renderer"
+
+module BrunocruzViewTool
+  # Your code goes here...
+end
---
+++ lib/brunocruz_view_tool/version.rb
@@ -0,0 +1,3 @@
+module BrunocruzViewTool
+  VERSION = "0.1.0"
+end
